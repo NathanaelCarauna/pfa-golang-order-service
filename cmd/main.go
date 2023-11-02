c6bdf7b3e531d2a2f71dab4c9dd1db9b848dbcaf
nathanael <nathanael.caruana@gmail.com>
2023-11-02 10:14:43 -0300

    feature: implement usecase getTotal to return total messages processed from rabbitmq

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,9 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"net/http"
 	"sync"
+	"time"
 
 	"github.com/nathanaelcarauna/pfa-go/internal/order/infra/database"
 	"github.com/nathanaelcarauna/pfa-go/internal/order/usecase"
@@ -23,6 +25,19 @@ func main() {
 	defer db.Close()
 	repository := database.NewOrderRepository(db)
 	uc := usecase.NewCalculateFinalPriceUseCase(repository)
+
+	http.HandleFunc("/total", func(w http.ResponseWriter, r *http.Request) {
+		uc := usecase.NewGetTotalUseCase(repository)
+		output, err := uc.Execute()
+		if err != nil {
+			//internal server error
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		json.NewEncoder(w).Encode(output)
+	})
+	go http.ListenAndServe(":8181", nil)
+
 	ch, err := rabbitmq.OpenChannel()
 	if err != nil {
 		panic(err)
@@ -31,23 +46,16 @@ func main() {
 	out := make(chan amqp.Delivery)
 	go rabbitmq.Consume(ch, out)
 
-	wg.Add(maxWorkers)
 	for i := 0; i < maxWorkers; i++ {
-		defer wg.Done()
-		go worker(out, uc, i)
+		wg.Add(1)
+		i := i
+		go func() {
+			fmt.Println("Starting worker", i)
+			defer wg.Done()
+			worker(out, uc, i)
+		}()
 	}
 	wg.Wait()
-
-	// input := usecase.OrderInputDTO{
-	// 	ID:    "1234",
-	// 	Price: 100,
-	// 	Tax:   10,
-	// }
-	// output, err := uc.Execute(input)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// println(output.FinalPrice)
 
 }
 
@@ -65,5 +73,6 @@ func worker(deliveryMessage <-chan amqp.Delivery, uc *usecase.CalculateFinalPric
 		}
 		msg.Ack(false)
 		fmt.Println("Worker", workerId, "processed order", input.ID)
+		time.Sleep(1 * time.Second)
 	}
 }
